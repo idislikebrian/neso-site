835f0d993b9f3f99f25c22512d1a08fd44a319b3
Brian Felix <fbrian100@gmail.com>
2021-12-14 13:22:21 -0500

    Connection to server

--- src/screens/Create.js
+++ src/screens/Create.js
@@ -14,7 +14,7 @@ const Create = () => {
 
         setIsPending(true);
 
-        fetch('http://localhost:8000/blogs', {
+        fetch('https://neso-server.herokuapp.com/blogs/', {
             method: 'POST',
             headers: {"Content-Type": "application/json"},
             body: JSON.stringify(blog)
