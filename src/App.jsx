c01bef758582063e2fdbe7074e1f6df179a4c479
Brian Felix <fbrian100@gmail.com>
2021-12-15 16:01:44 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,12 @@ import Bulletin from "./components/bulletin/Bulletin";
 import "./styles/app.scss";
 
 function App() {
+  console.log(
+    process.env.REACT_APP_SPREADSHEET_ID,
+    process.env.REACT_APP_SHEET_ID,
+    process.env.REACT_APP_CLIENT_EMAIL, 
+    process.env.REACT_APP_PRIVATE_KEY
+    )
   return (
     <div className="app">
      <Topbar/>
