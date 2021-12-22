385e7713cc3b3f13681bab24c75fd3ec726ccf73
Brian Felix <fbrian100@gmail.com>
2021-12-21 19:27:02 -0500

    Adding footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,25 @@ import Topbar from "./components/topbar/Topbar";
 import Rules from "./components/rules/Rules";
 import Leaderboard from "./components/leaderboard/Leaderboard";
 import Bulletin from "./components/bulletin/Bulletin";
+import Footer from "./components/footer/Footer";
 import "./styles/app.scss";
 
 function App() {
   return (
     <div className="app">
      <Topbar/>
-     <Grid container>
-    <Grid xs={12} md={12} lg={4}>
-    <Leaderboard/>
-    </Grid>
-    <Grid xs={12} md={12} lg={4}>
-    <Bulletin/>
-    </Grid>
-    <Grid xs={12} md={12} lg={4}>
-    <Rules/>
-    </Grid>
-     </Grid>
-     
+      <Grid container>
+        <Grid xs={12} md={12} lg={4}>
+        <Leaderboard/>
+        </Grid>
+        <Grid xs={12} md={12} lg={4}>
+        <Bulletin/>
+        </Grid>
+        <Grid xs={12} md={12} lg={4}>
+        <Rules/>
+        </Grid>
+      </Grid>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../../../src/styles/footer.scss"
+import { Link } from 'react-router-dom';
+
+export default function Topbar() {
+
+    return (
+        <div className="footer">
+            <p>Powered by <a href="https://straighttotell.com">Straight to Tell</a></p>
+        </div>
+    )
+}
